ba33f73572fc26296873ce4b19188b690b8e6ecf
RMata04 <130968710+RMata04@users.noreply.github.com>
2023-04-17 18:05:24 +0200

    Delete Constantes.java

--- Constantes.java
+++
@@ -1,9 +0,0 @@
-package edificio;
-
-public interface Constantes {
-double IMPUESTOPROP=0.12;
-double IMPUESTOINQ1=0.11;
-double IMPUESTOINQ2=0.05;
-double BASEPROP=450;
-abstract double CalculoImpuesto();
-}
